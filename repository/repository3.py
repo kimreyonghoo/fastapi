6ae79fa968120fadce911bafbcc3184edcf177dc
kimreyonghoo <krh200058@gmail.com>
2025-05-07 19:01:20 +0900

    user

--- repository/repository3.py
+++ repository/repository3.py
@@ -13,11 +13,40 @@ nutr_db=['에너지', '탄수화물', '식이섬유', '단백질', '리놀레산
       '비타민A', '비타민D', '비타민E', '비타민K', '비타민C', '비타민B1', '비타민B2', '나 이아신', '비타민B6', 
       '비타민B12', '엽산', '판토텐산', '비오틴', '칼슘', '인', '나트륨', '염소', '칼륨', '마그네슘', '철', 
       '아연', '구리', '망간', '요 오드', '셀레늄', '몰리브덴', '크롬']
-get_table("user",aws_access)
-def calculate_bmr(sex, weight, height, age):
+
+
+def update_user_nutrition(user_id, new_physique, new_nutr_vector, table_name='user'):
+    """
+    DynamoDB user 테이블의 physique와 nutr_db 항목을 업데이트
+    """
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
+    try:
+        response = table.update_item(
+            Key={
+                'user_id': user_id
+            },
+            UpdateExpression="SET physique = :p, meal.nutr_db = :n",
+            ExpressionAttributeValues={
+                ':p': new_physique,
+                ':n': new_nutr_vector
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+        print("업데이트 완료:", response['Attributes'])
+        return response['Attributes']
+    except Exception as e:
+        print("오류 발생:", e)
+        return None
+user=get_table("user",aws_access)
+def calculate_bmr(sex, weight, height, age,user:dict):
       #기초대사량 계산
-    if sex.lower() == 'male':
-        return 10 * weight + 6.25 * height - 5 * age + 5
-    else:
-        return 10 * weight + 6.25 * height - 5 * age - 161
+      bmr=0
+      if sex.lower() == 'male':
+            bmr=10 * weight + 6.25 * height - 5 * age + 5
+      else:
+            bmr=10 * weight + 6.25 * height - 5 * age - 161
+      tdee=bmr*user.pysique.actyvity_level
+
   
